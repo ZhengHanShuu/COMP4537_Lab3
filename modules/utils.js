96184462094f552043e1db7ca68270c1f8e32cb8
JacksonHu <hjacksonhjh@gmail.com>
2024-02-04 17:44:01 -0800

    updated code

--- modules/utils.js
+++ modules/utils.js
@@ -3,7 +3,7 @@ const messages = require('../lang/en/en');
 
 function getDate(name) {
   const now = new Date();
-  //return `Hello ${name}, What a beautiful day. Server current date and time is ${now.toString()}`;
+  return `Hello ${name}, What a beautiful day. Server current date and time is ${now.toString()}`;
 }
 
 module.exports = { getDate };
