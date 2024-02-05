8f478a60c0266fccae6a4a007ec092c01ae002a6
JacksonHu <hjacksonhjh@gmail.com>
2024-02-04 17:58:32 -0800

    updated code

--- modules/utils.js
+++ modules/utils.js
@@ -1,10 +1,8 @@
 // ./modules/utils.js
 const messages = require('../lang/en/en');
-const moment = require('moment-timezone');
 
 function getDate(name) {
-  //const now = new Date();
-  const now = moment().tz('America/Los_Angeles');
+  const now = new Date();
   return `Hello ${name}, What a beautiful day. Server current date and time is ${now.toString()}`;
 }
 
